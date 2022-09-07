1b4bda4c0a685ed9d42c266a4a396eaa2adbab1e
ANDRES VALENCIA <andresvalenciaarango@gmail.com>
2022-09-06 21:42:09 -0500

    Esta listo el servicio y el templete actualizar empresa

--- src/main/java/com/proyectodePruebaUdeA/ciclo3/controller/ControllerFull.java
+++ src/main/java/com/proyectodePruebaUdeA/ciclo3/controller/ControllerFull.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,4 +39,20 @@ public class ControllerFull {
         }
         return "redirect:/AgregarEmpresa";
     }
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id){
+        Empresa emp=empresaService.getEmpresaById(id);
+        model.addAttribute("emp",emp);
+        return "editarEmpresa";
+    }
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp){
+        if(empresaService.saveOrUpdateEmpresa(emp)){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
+
+    }
+
 }
